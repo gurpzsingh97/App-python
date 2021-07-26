cbea7f790b8f15b384e05cc66c628a806ef2b29c
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2021-07-26 20:04:24 +0100

    registration page done

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from flask import (
     Flask, flash, render_template, 
-    redirect, session, url_for)
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 if os.path.exists("env.py"):
@@ -23,6 +24,28 @@ mongo = PyMongo(app)
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration succesful!!")
+
+    return render_template("register.html")
+
 
 
 if __name__ == "__main__":
