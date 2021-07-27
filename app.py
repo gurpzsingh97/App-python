9b8a8e39b3ba7ba0958140b4efcf79c85105081e
gurpzsingh97 <79270339+gurpzsingh97@users.noreply.github.com>
2021-07-27 14:40:31 +0100

    log in and log out done

--- app.py
+++ app.py
@@ -43,8 +43,53 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration succesful!!")
+        return redirect((url_for("profile", username=session["user"])))
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}" .format(
+                        request.form.get("username")))
+                    return redirect((url_for(
+                        "profile", username=session["user"])))
+            else:
+                flash("Incorrect Username or Password")
+                return redirect(url_for("login"))
+
+        
+        else:
+            flash("Username or Password is incorrect")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    flash("You've been logged out succesfully")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
 
